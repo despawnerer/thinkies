2a04b2466b7919c65f221708b4ae78e47506158a
Алексей Шельф <despawn@gmail.com>
2015-01-02 00:58:59 +0300

    Properly implement logging in and out of the site

--- thoughts/urls.py
+++ thoughts/urls.py
@@ -6,7 +6,9 @@ from .views import Index
 urlpatterns = [
     url(r'^admin/', include(admin.site.urls)),
 
-    url(r'^auth/', include('social.apps.django_app.urls', namespace='social')),
+    url(r'^auth/logout/', 'django.contrib.auth.views.logout', name='logout'),
+    url(r'^auth/', include(
+        'social.apps.django_app.urls', namespace='social')),
 
     url(r'^$', Index.as_view()),
 ]
