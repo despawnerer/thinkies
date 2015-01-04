47da30a9b76cf49900b7282d4e0f37bab9ec7f5f
Алексей Шельф <despawn@gmail.com>
2015-01-04 23:50:10 +0300

    Switch to postgresql

--- thoughts/settings.py
+++ thoughts/settings.py
@@ -70,10 +70,14 @@ WSGI_APPLICATION = 'thoughts.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'thoughts',
+        'USER': 'thoughts',
+        'PASSWORD': 'thisreallydoesntmatter',
+        'HOST': ''
     }
 }
+
 
 AUTHENTICATION_BACKENDS = (
     'social.backends.twitter.TwitterOAuth',
