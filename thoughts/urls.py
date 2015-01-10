7f744086823e5bb7123d95e7df715f0996681a25
Алексей Шельф <despawn@gmail.com>
2015-01-11 01:42:50 +0300

    Add a shitty language chooser

--- thoughts/urls.py
+++ thoughts/urls.py
@@ -2,6 +2,8 @@ from django.conf.urls import include, url
 
 
 urlpatterns = [
+    url(r'^i18n/', include('django.conf.urls.i18n')),
+
     url(r'^auth/logout/', 'django.contrib.auth.views.logout', name='logout'),
     url(r'^auth/', include(
         'social.apps.django_app.urls', namespace='social')),
