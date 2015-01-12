4cc578a3dadabc835e4f019cb9e627a0b37ff80d
Алексей Шельф <despawn@gmail.com>
2015-01-12 23:55:50 +0300

    Index titles in more languages

--- movies/search_indexes.py
+++ movies/search_indexes.py
@@ -2,7 +2,7 @@ from haystack import indexes
 
 from thinkies.search.fields import CharField
 
-from .models import Movie, TitleTranslation
+from .models import Movie
 
 
 class MovieIndex(indexes.SearchIndex, indexes.Indexable):
@@ -11,6 +11,18 @@ class MovieIndex(indexes.SearchIndex, indexes.Indexable):
     year = indexes.IntegerField(model_attr='year')
     title_en = CharField(analyzer='english')
     title_ru = CharField(analyzer='russian')
+    title_fr = CharField(analyzer='french')
+    title_es = CharField(analyzer='spanish')
+    title_de = CharField(analyzer='german')
+    title_nl = CharField(analyzer='dutch')
+    title_sv = CharField(analyzer='swedish')
+    title_hi = CharField(analyzer='hindi')
+    title_no = CharField(analyzer='norwegian')
+    title_nb = CharField(analyzer='norwegian')
+    title_nn = CharField(analyzer='norwegian')
+    title_pt = CharField(analyzer='portuguese')
+    title_it = CharField(analyzer='italian')
+    title_da = CharField(analyzer='danish')
 
     def get_model(self):
         return Movie
@@ -23,3 +35,39 @@ class MovieIndex(indexes.SearchIndex, indexes.Indexable):
 
     def prepare_title_ru(self, obj):
         return obj.titles_by_language.get('ru') or ''
+
+    def prepare_title_fr(self, obj):
+        return obj.titles_by_language.get('fr') or ''
+
+    def prepare_title_es(self, obj):
+        return obj.titles_by_language.get('es') or ''
+
+    def prepare_title_de(self, obj):
+        return obj.titles_by_language.get('de') or ''
+
+    def prepare_title_nl(self, obj):
+        return obj.titles_by_language.get('nl') or ''
+
+    def prepare_title_sv(self, obj):
+        return obj.titles_by_language.get('sv') or ''
+
+    def prepare_title_hi(self, obj):
+        return obj.titles_by_language.get('hi') or ''
+
+    def prepare_title_no(self, obj):
+        return obj.titles_by_language.get('no') or ''
+
+    def prepare_title_nb(self, obj):
+        return obj.titles_by_language.get('nb') or ''
+
+    def prepare_title_nn(self, obj):
+        return obj.titles_by_language.get('nn') or ''
+
+    def prepare_title_pt(self, obj):
+        return obj.titles_by_language.get('pt') or ''
+
+    def prepare_title_it(self, obj):
+        return obj.titles_by_language.get('it') or ''
+
+    def prepare_title_da(self, obj):
+        return obj.titles_by_language.get('da') or ''
