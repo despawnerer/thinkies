9e9f7ef855ca0c1b9cf34caada2ae05f31c647bf
Алексей Шельф <despawn@gmail.com>
2015-04-27 20:16:57 +0700

    Add a first not-really-working settings page

--- website/urls.py
+++ website/urls.py
@@ -3,10 +3,12 @@ from django.conf.urls import url
 from .views.index import Index
 from .views.search import SearchView
 from .views.movie import MovieView
+from .views.settings import SettingsView
 
 
 urlpatterns = [
     url(r'^$', Index.as_view(), name='index'),
     url(r'^search/$', SearchView.as_view(), name='search'),
     url(r'^movies/(?P<pk>\d+)/$', MovieView.as_view(), name='movie'),
+    url(r'^settings/$', SettingsView.as_view(), name='settings'),
 ]
---
+++ website/views/settings.py
@@ -0,0 +1,44 @@
+from django.views.generic import TemplateView
+from django.utils.translation import ugettext_lazy as _
+
+from social.backends.utils import load_backends
+from social.apps.django_app.utils import BACKENDS
+
+
+PROVIDER_NAMES = {
+    'facebook': 'facebook',
+    'twitter': 'twitter',
+}
+
+PROVIDER_VERBOSE_NAMES = {
+    'facebook': _("Facebook"),
+    'twitter': _("Twitter"),
+}
+
+
+class SettingsView(TemplateView):
+    template_name = 'pages/settings.html'
+
+    def get_context_data(self, **kwargs):
+        identities = self.request.user.identities.all()
+        provider_details = self.get_provider_details(identities)
+        context = {
+            'identities': identities,
+            'provider_details': provider_details,
+        }
+        context.update(kwargs)
+        return context
+
+    def get_provider_details(self, identities):
+        associated_backends = {i.provider for i in identities}
+        backends = load_backends(BACKENDS)
+        provider_details = []
+        for backend_name in backends.keys():
+            provider_details.append({
+                'backend_name': backend_name,
+                'name': PROVIDER_NAMES.get(backend_name, backend_name),
+                'verbose_name': PROVIDER_VERBOSE_NAMES.get(
+                    backend_name, backend_name),
+                'is_associated': backend_name in associated_backends,
+            })
+        return provider_details
